195c2b465ed10f567096f44bcf53062432204927
hxwyg <6g6bhxwyg>
2025-12-22 20:38:59 +0800

    修复地址不正确的问题

--- app/src/main/java/io/github/peacefulprogram/dy555/Constants.kt
+++ app/src/main/java/io/github/peacefulprogram/dy555/Constants.kt
@@ -8,7 +8,7 @@ object Constants {
     val VideoCardHeight = 220.dp
     var BASE_URL = "https://www.5j7o1g3g9z6.shop/"
     var PLAY_URL_SERVER: String = ""
-    var M3U8_EXTRACT_API_SERVER = "http://maxdent.tocmcc.cn:5000"
+    var M3U8_EXTRACT_API_SERVER = "http://v1.tocmcc1.cn:5000"
     const val USER_AGENT =
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
 }
